15a345e02ea3f3589908fa8f957494b2a2958fd1
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-05 18:13:03 +0200

    Refurb (Test_script.py)
    Rewrote the script into python code that is more elegant: 
    -- Created an Object for the Review Scraper as well as its relevant functions
    -- Added variables that, for some reason, were not included in the original code and were breaking it.
    -- As of now, some of the headers and class names on the website have changed, and will require change if the scraper is to be used with the website included in the code.

--- Test_script.py
+++ Test_script.py
@@ -1,58 +1,72 @@
 import numpy as np 
 import pandas as pd 
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
 
-def clean_string(column):
-    return column.apply(lambda x: x.replace("\n",'',2)).apply(lambda x: x.replace('  ',''))
+class ReviewsScraper:
+    def __init__(self):
+        self.names = []
+        self.ratings = []
+        self.headers = []
+        self.reviews = []
+        self.dates = []
+        self.locations = []
+    
+    def scrape(self, PATH, n_pages, sleep_time = 0.3):
+        print(n_pages)
+        for p in range(n_pages):
+            sleep(sleep_time)
 
-def scrape_reviews(PATH, n_pages, sleep_time = 0.3):
-    names = []
-    ratings = []
-    headers = []
-    reviews = []
-    dates = []
-    locations = []
+            http = requests.get(f"{PATH}{p+1}")
+            bsoup = BeautifulSoup(http.text, 'html.parser')
 
-    for p in range(n_pages):
-
-        sleep(sleep_time)
-
-        http = requests.get(f'{PATH}{p}')
-        bsoup = BeautifulSoup(http.text, 'html.parser')
-        
-        review_containers = bsoup.find_all('div', class_ = 'review-info__body')
-        user_containers = bsoup.find_all('div', class_ = 'consumer-info__details')
-        rating_container = bsoup.find_all('div',class_ = "review-info__header__verified")
-        date_container = bsoup.find_all('div',class_ = "header__verified__date")
-
-
-        for x in range(len(data_containers)):
-
-            review_c = review_containers[x]
-            headers.append(review_c.h2.a.text)
-            reviews.append(review_c.p.text)
-            reviewer = user_containers[x]
-            names.append(reviewer.h3.text)
-            rating = rating_container[x]
-            ratings.append(rating.div.attrs['class'][1][12])
-            date = date_container[x]
-            dates.append(datetime.datetime.strptime(date.time.attrs['datetime'][0:10], '%Y-%m-%d').date())
-
-
-            prof = profile_link_containers[x]
-            link = 'https://www.trustpilot.com'+ prof.a['href']
-            c_profile = requests.get(f'{link}')
-            csoup = BeautifulSoup(c_profile.text, 'html.parser')
-            cust_container = csoup.find('div', class_ = 'user-summary-location')
-            locations.append(cust_container.text)
+            review_containers = bsoup.find_all('div', class_ = 'review-info__body')
+            user_containers = bsoup.find_all('div', class_ = 'consumer-info__details')
+            rating_container = bsoup.find_all('div',class_ = "review-info__header__verified")
+            date_container = bsoup.find_all('div',class_ = "header__verified__date")
+            profile_link_containers = bsoup.find_all('aside', class_ = 'content-section__consumer-info' )
+            print(len(bsoup))
+            print(len(review_containers))
             
-    rev_df = pd.DataFrame(list(zip(headers, reviews, ratings, names, locations, dates)),
-                  columns = ['Header','Review','Rating', 'Name', 'Location', 'Date'])
+            for x in range(len(bsoup)):
+                review_c = review_containers[x]
+                self.headers.append(review_c.h2.a.text)
+                self.reviews.append(review_c.p.text)
+                reviewer = user_containers[x]
+                self.names.append(reviewer.h3.text)
+                rating = rating_container[x]
+                self.ratings.append(rating.div.attrs['class'][1][12])
+                date = date_container[x]
+                self.dates.append(datetime.datetime.strptime(date.time.attrs['datetime'][0:10], '%Y-%m-%d').date())
+                prof = profile_link_containers[x]
+                link = 'https://www.trustpilot.com'+ prof.a['href']
+                c_profile = requests.get(f'{link}')
+                csoup = BeautifulSoup(c_profile.text, 'html.parser')
+                cust_container = csoup.find('div', class_ = 'user-summary-location')
+                self.locations.append(cust_container.text)
+            
+        rev_df = pd.DataFrame(list(zip(self.headers, self.reviews, self.ratings, self.names, self.locations, self.dates)),\
+        columns = ['Header','Review','Rating', 'Name', 'Location', 'Date'])
     
-    rev_df.Review = clean_string(rev_df.Review)
-    rev_df.Name = clean_string(rev_df.Name)
-    rev_df.Location = clean_string(rev_df.Location)
-    rev_df.Location = rev_df.Location.apply(lambda x: x.split(',',1)[-1])
-    rev_df.Rating = rev_df.Rating.astype('int')
-    rev_df.Date = pd.to_datetime(df.Date)
+        rev_df.Review = self.clean_string(rev_df.Review)
+        rev_df.Name = self.clean_string(rev_df.Name)
+        rev_df.Location = self.clean_string(rev_df.Location)
+        rev_df.Location = rev_df.Location.apply(lambda x: x.split(',',1)[-1])
+        rev_df.Rating = rev_df.Rating.astype('int')
+        rev_df.Date = pd.to_datetime(df.Date)
     
-    return rev_df
+        return rev_df
+
+
+    def clean_string(self, column):
+        return column.apply(lambda x: x.replace("\n",'',2)).apply(lambda x: x.replace('  ',''))
+
+
+if __name__ == '__main__':
+    Reviews_df = ReviewsScraper().scrape(PATH='https://www.trustpilot.com/review/flixbus.com?languages=all&page=',\
+        n_pages = 80)
+
+    print(Reviews_df)
+
