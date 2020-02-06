c1d95e66fc9d9a4af1892c3635ad2f106ccc933a
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-06 15:08:54 +0200

    Refurb (selenium_script.py)
    -- Refurbished code to create a scraper object

--- selenium_script.py
+++ selenium_script.py
@@ -1,28 +1,31 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import Firefox
 import pandas as pd
 
-webdriver = "path_to_installed_driver_location"
+class CHOOCHOO_Scraper:
+    def __init__(self):
+        self.webdriver = "./"
+        self.driver = Firefox(self.webdriver)
 
-driver = Chrome(webdriver)
+    def scrape(self, pages, pre_url):
+        for page in range(0,pages):
+            url = pre_url + str(page+1)
+            self.driver.get(url)
+            items = len(self.driver.find_elements_by_class_name("quote"))
+            total = []
+            for item in range(items):
+                quotes = self.driver.find_elements_by_class_name("quote")
+                for quote in quotes:
+                    quote_text = quote.find_element_by_class_name('text').text
+                    author = quote.find_element_by_class_name('author').text
+                    new = ((quote_text,author))
+                    total.append(new)
+            df = pd.DataFrame(total,columns=['quote','author'])
+            # df.to_csv('quoted.csv')
+        self.driver.close()
+        return df
 
-pages = 10
-
-for page in range(0,pages):
-
-    url = "https://www.g2.com/products/trello/reviews?page=" + str(page+1) + "/"
-
-    driver.get(url)
-
-    items = len(driver.find_elements_by_class_name("quote"))
-
-    total = []
-    for item in range(items):
-        quotes = driver.find_elements_by_class_name("quote")
-        for quote in quotes:
-            quote_text = quote.find_element_by_class_name('text').text
-            author = quote.find_element_by_class_name('author').text
-            new = ((quote_text,author))
-            total.append(new)
-    df = pd.DataFrame(total,columns=['quote','author'])
-    df.to_csv('quoted.csv')
-driver.close()
+if __name__ == '__main__':
+    pre_url = "https://www.g2.com/products/trello/reviews?page="
+    pages = 1
+    Trello_df = CHOOCHOO_Scraper().scrape(pages, pre_url)
+    print(Trello_df)
