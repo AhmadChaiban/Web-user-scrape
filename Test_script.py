88593b4888157ab3fbe2e2d025344e1ce962f093
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-02-06 14:38:46 +0200

    Feat (security_breacher - Test_script.py):
    -- Added a security preacher that attempts to establish a connection through proxy. To no avail so far. 
    
    Feat (get_new_ip - Test_script.py): 
    -- Attempted to use Stem to give a new ip after every attempted request. So far it won't connect through a socket. I don't think this will fix it either because even the first request doesn't work.

--- Test_script.py
+++ Test_script.py
@@ -4,6 +4,8 @@ from time import sleep
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
+from stem import Signal
+from stem.control import Controller
 
 class ReviewsScraper:
     def __init__(self):
@@ -13,23 +15,60 @@ class ReviewsScraper:
         self.reviews = []
         self.dates = []
         self.locations = []
+
+    def security_breacher(self, http):
+        print(http)
+        proxies = {'http':  'socks5://127.0.0.1:9050',
+                   'https': 'socks5://127.0.0.1:9050'}
+ 
+        s = requests.Session()
+        s.proxies = proxies
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+            }
+        html = s.get(http, headers=headers).content
+        return html
+
+    def get_new_ip(self):
+        with Controller.from_port(address = '127.0.0.1', port=9051) as controller:
+            controller.authenticate()
+            controller.signal(Signal.NEWNYM)
+            sleep(controller.get_newnym_wait())
     
     def scrape(self, PATH, n_pages, sleep_time = 0.3):
-        print(n_pages)
         for p in range(n_pages):
             sleep(sleep_time)
+            # self.get_new_ip()            
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'}
+            http = requests.get(f"{PATH}{p+1}", headers= headers, stream=False)
+            # http = self.security_breacher(http)
+            # http = PATH + str(p+1)
+            print(f"{http.text}")
 
-            http = requests.get(f"{PATH}{p+1}")
             bsoup = BeautifulSoup(http.text, 'html.parser')
-
-            review_containers = bsoup.find_all('div', class_ = 'review-info__body')
-            user_containers = bsoup.find_all('div', class_ = 'consumer-info__details')
-            rating_container = bsoup.find_all('div',class_ = "review-info__header__verified")
-            date_container = bsoup.find_all('div',class_ = "header__verified__date")
-            profile_link_containers = bsoup.find_all('aside', class_ = 'content-section__consumer-info' )
+            sleep(5)
+            heading_containers = bsoup.find_all('div', class_ = 'box__heading box__heading--bordered p-1')
+            ## Review body: reviewBody
+            ## Check if you need to click the show more
+            review_containers = bsoup.find_all('div', class_ = 'p-1')
+            sleep(5)
+            ## Generalize the rating boxes
+            rating_container = bsoup.find_all('div',class_ = "stars stars--secondary large xlarge--medium-down stars-10")
+            sleep(5)
+            ## Want the date from inside this
+            date_container = bsoup.find_all('div',class_ = "x-current-review-date")
+            sleep(5)
+            ## We want the href of this 
+            profile_link_containers = bsoup.find_all('aside', class_ = 'mr-1' )
+            sleep(5)
             print(len(bsoup))
             print(len(review_containers))
-            
+
+            print(rating_container)
+            print(heading_containers)
+
+            print(bsoup)
+            return
             for x in range(len(bsoup)):
                 review_c = review_containers[x]
                 self.headers.append(review_c.h2.a.text)
@@ -59,14 +98,11 @@ class ReviewsScraper:
     
         return rev_df
 
-
     def clean_string(self, column):
         return column.apply(lambda x: x.replace("\n",'',2)).apply(lambda x: x.replace('  ',''))
 
-
 if __name__ == '__main__':
-    Reviews_df = ReviewsScraper().scrape(PATH='https://www.trustpilot.com/review/flixbus.com?languages=all&page=',\
-        n_pages = 80)
-
-    print(Reviews_df)
+    Trello_df = ReviewsScraper().scrape(PATH='https://www.g2.com/products/trello/reviews?page=',\
+                                         n_pages = 1)
+    print(Trello_df)
 
